94f273ca79e7969e03285c8f1fc809a390fdcae4
Alena Bulanova <kxzws54@gmail.com>
2022-10-19 22:13:47 +0300

    refactor: move table styles to separate file

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,50 +1,9 @@
 import { useEffect } from 'react';
-import styled from 'styled-components';
 import { ITableProps } from '../../types/interfaces';
 import visualizeBigDigit from '../../utils/visualizeBigDigit';
 import baseTheme from '../../theme';
 import * as F from '../../styled/Fonts';
-
-const StyledTableCont = styled.section`
-  margin: 0 20px;
-  background-color: #fff;
-  border: 1px solid ${({ theme }) => theme.colors.secondary};
-  border-radius: 6px;
-  box-shadow: rgb(0 0 0 / 40%) 0px 2px 15px -3px;
-`;
-
-const StyledTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-`;
-
-const TableHead = styled.thead`
-  background-color: rgba(0, 0, 0, 0.03);
-  border-radius: 6px;
-`;
-
-const TableBody = styled.tbody``;
-
-const TableRow = styled.tr`
-  border-top: 1px solid ${({ theme }) => theme.colors.secondary};
-  transition: background-color ${({ theme }) => theme.durations.ms150}ms ease;
-
-  tbody & {
-    cursor: pointer;
-  }
-
-  tbody &:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-`;
-
-const TableCell = styled.td`
-  padding: 16px;
-
-  &:nth-of-type(n + 2) {
-    text-align: right;
-  }
-`;
+import * as T from './styled';
 
 const Table = (props: ITableProps) => {
   const { data, loadData, isLoading } = props;
@@ -54,34 +13,26 @@ const Table = (props: ITableProps) => {
   }, [loadData]);
 
   return (
-    <StyledTableCont>
-      <StyledTable>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <F.Text2>Название</F.Text2>
-            </TableCell>
-            <TableCell>
-              <F.Text2>Стоимость</F.Text2>
-            </TableCell>
-            <TableCell>
-              <F.Text2>Капитализация</F.Text2>
-            </TableCell>
-            <TableCell>
-              <F.Text2>Средняя стоимость (24 ч.)</F.Text2>
-            </TableCell>
-            <TableCell>
-              <F.Text2>Предложение</F.Text2>
-            </TableCell>
-            <TableCell>
-              <F.Text2>Объём (24 ч.)</F.Text2>
-            </TableCell>
-            <TableCell>
-              <F.Text2>Изменение (24 ч.)</F.Text2>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+    <T.StyledTableCont>
+      <T.StyledTable>
+        <T.TableHead>
+          <T.TableRow>
+            {[
+              'Название',
+              'Стоимость',
+              'Капитализация',
+              'Средняя стоимость (24 ч.)',
+              'Предложение',
+              'Объём (24 ч.)',
+              'Изменение (24 ч.)',
+            ].map((name) => (
+              <T.TableCell>
+                <F.Text2>{name}</F.Text2>
+              </T.TableCell>
+            ))}
+          </T.TableRow>
+        </T.TableHead>
+        <T.TableBody>
           {data.map((item) => {
             const price = Number(parseFloat(item.priceUsd).toFixed(2));
             const marketCap = Number(parseFloat(item.marketCapUsd).toFixed(2));
@@ -93,26 +44,26 @@ const Table = (props: ITableProps) => {
             const isPositive = percent24h > 0;
 
             return (
-              <TableRow key={item.id}>
-                <TableCell>
+              <T.TableRow key={item.id}>
+                <T.TableCell>
                   <F.Subtitle>{item.name}</F.Subtitle>
-                </TableCell>
-                <TableCell>${price}</TableCell>
-                <TableCell>${visualizeBigDigit(marketCap)}</TableCell>
-                <TableCell>${vwap24h}</TableCell>
-                <TableCell>{visualizeBigDigit(supply)}</TableCell>
-                <TableCell>${visualizeBigDigit(volume24h)}</TableCell>
-                <TableCell>
+                </T.TableCell>
+                <T.TableCell>${price}</T.TableCell>
+                <T.TableCell>${visualizeBigDigit(marketCap)}</T.TableCell>
+                <T.TableCell>${vwap24h}</T.TableCell>
+                <T.TableCell>{visualizeBigDigit(supply)}</T.TableCell>
+                <T.TableCell>${visualizeBigDigit(volume24h)}</T.TableCell>
+                <T.TableCell>
                   <F.Text1 color={isPositive ? baseTheme.colors.success : baseTheme.colors.error}>
                     {percent24h}%
                   </F.Text1>
-                </TableCell>
-              </TableRow>
+                </T.TableCell>
+              </T.TableRow>
             );
           })}
-        </TableBody>
-      </StyledTable>
-    </StyledTableCont>
+        </T.TableBody>
+      </T.StyledTable>
+    </T.StyledTableCont>
   );
 };
 
---
+++ src/components/Table/styled.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const StyledTableCont = styled.section`
+  margin: 0 20px;
+  background-color: #fff;
+  border: 1px solid ${({ theme }) => theme.colors.secondary};
+  border-radius: 6px;
+  box-shadow: rgb(0 0 0 / 40%) 0px 2px 15px -3px;
+`;
+
+export const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+export const TableHead = styled.thead`
+  background-color: rgba(0, 0, 0, 0.03);
+  border-radius: 6px;
+`;
+
+export const TableBody = styled.tbody``;
+
+export const TableRow = styled.tr`
+  border-top: 1px solid ${({ theme }) => theme.colors.secondary};
+  transition: background-color ${({ theme }) => theme.durations.ms150}ms ease;
+
+  tbody & {
+    cursor: pointer;
+  }
+
+  tbody &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const TableCell = styled.td`
+  padding: 16px;
+
+  &:nth-of-type(n + 2) {
+    text-align: right;
+  }
+`;
