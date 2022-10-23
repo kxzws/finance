bf7b15a36857e0c941639bc5c91e41714b9b74f5
Alena Bulanova <kxzws54@gmail.com>
2022-10-23 19:10:24 +0300

    fix: change settings for deploy

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -3,8 +3,7 @@ import axios from 'axios';
 const requestTimeout = 5000;
 
 export const coincapInstance = axios.create({
-  // baseURL: `${process.env.REACT_APP_API_URL}`,
-  baseURL: 'https://api.coincap.io/v2/',
+  baseURL: `${process.env.REACT_APP_API_URL}`,
   timeout: requestTimeout,
 });
 
